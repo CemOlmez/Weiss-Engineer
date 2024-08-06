3dc878120ab4573b4489e1ea3e897022ac33371e
CemOlmez <c.olmez123@gmail.com>
2024-08-07 01:09:16 +0300

    Update 2024/08

---
+++ codeTester/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import React from 'react';
+import './App.css'
+import StarryBackground from './components/StarryBackground';
+
+function App() {
+  return (
+    <>
+      <div>
+      <StarryBackground />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ codeTester/src/components/StarryBackground.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './StarryBackground.css'; // Stylesheet for the starry background
+
+const StarryBackground = () => {
+  return (
+    <div className="starry-background">
+      {/* Stars will be added here */}
+    </div>
+  );
+};
+
+export default StarryBackground;
